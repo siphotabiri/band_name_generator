5c4549d6449fad3327beef02ad8a6d87445b4947
Siyavuya Mankanku <siphokwesi1@outlook.com>
2023-03-15 23:25:01 +0000

    version one

---
+++ main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Hello, Future Software Developer!")
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?")
+#3. Ask the user for the name of a pet.
+pet_name = input("What was the name of your first pet?")
+#4. Combine the name of their city and pet and show them their band name.
+new_band_name = print("Your new band name is " + city + " " + pet_name)
